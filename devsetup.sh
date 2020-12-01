398510b7926694a8d4aa76a51bb65695ed0af4d3
Roger Mukai <10594369+MukaiGuy@users.noreply.github.com>
2020-12-01 20:43:56 +0000

    Update devsetup.sh

--- devsetup.sh
+++ devsetup.sh
@@ -1,4 +1,4 @@
-!#/bin/bash 
+#!/bin/bash 
 
 sudo apt-get update && sudo apt full-upgrade -y && sudo ap-get install -y zsh git curl python3-venv python3-pip python3.8 && sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && sudo apt install fzf
 
