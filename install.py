facc583af8bf05158c5a5c1144465a7e067b06e0
Roger Mukai <rmukai@Roger-Mukai-Mac.local>
2023-05-04 14:40:49 -0400

    updated the install.py script

--- install.py
+++ install.py
@@ -5,9 +5,9 @@ system = platform.system()
 
 if system == 'Linux': # Execute a Linux command
     print("This is a Linux machine.")
-    os.system('pwd')  # Example command
+    os.system('./devsetup.sh')  # Example command
 elif system == 'Darwin': # Execute a macOS command
     print("This is a MacOS machine.")
-    os.system('pwd')  # Example command
+    os.system('./macsetup.sh')  # install command
 else:
     print("Unsupported operating system. If you are on a Windows machine please use wsl2. \n Docs: https://learn.microsoft.com/en-us/windows/wsl/install")
