57db92487a0aad0cbf5c53bb212c10530d9124b4
Roger Mukai <10594369+MukaiGuy@users.noreply.github.com>
2025-01-30 12:07:05 -0500

    Update Auto_install.sh

--- Auto_install.sh
+++ Auto_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if echo uname = 'Linux'; then
-    echo 'The Linux script is starting please watch for prompts' && sleep 5 && ./devsetup.sh && ./personalize.sh
+    echo 'The Linux script is starting please watch for prompts' && sleep 5 && ./devsetup.sh &&  sleep 5 && ./personalize.sh
 else
-    echo 'The MacOS script is starting please watch for prompt' && sleep 5 && ./macsetup.sh && ./personalize.sh
+    echo 'The MacOS script is starting please watch for prompt' && sleep 5 && ./macsetup.sh &&  sleep 5 && ./personalize.sh
 fi
