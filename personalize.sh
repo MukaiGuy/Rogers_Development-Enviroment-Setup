f5ab3b742de3e80d7ca9167df487f63912efaba4
Roger Mukai <10594369+MukaiGuy@users.noreply.github.com>
2025-01-30 12:05:01 -0500

    Updated personalize.sh
    To use the $HOME variable

--- personalize.sh
+++ personalize.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
- cp mukaiguy.zsh-theme /home/rmukai/.oh-my-zsh/themes/mukaiguy.zsh-theme && sudo rm /home/rmukai/.zshrc && cp zshrc /home/rmukai/.zshrc && cp nanorc.conf /etc/nanorc &&source /home/rmukai/.zshrc
+ cp mukaiguy.zsh-theme $HOME/.oh-my-zsh/themes/mukaiguy.zsh-theme && sudo rm $HOME/.zshrc && cp zshrc $HOME/.zshrc && cp nanorc.conf /etc/nanorc && source $HOME/.zshrc
