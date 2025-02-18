1865cf274a76bc6a485c5f0d68a12a6d4684b8ac
Roger Mukai <10594369+MukaiGuy@users.noreply.github.com>
2025-02-18 13:14:21 -0500

    Updated personalize.sh

--- personalize.sh
+++ personalize.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
- cp mukaiguy.zsh-theme $HOME/.oh-my-zsh/themes/mukaiguy.zsh-theme && sudo rm $HOME/.zshrc && cp zshrc $HOME/.zshrc && cp nanorc.conf /etc/nanorc && source $HOME/.zshrc
+ cp mukaiguy.zsh-theme $HOME/.oh-my-zsh/themes/mukaiguy.zsh-theme && sudo rm $HOME/.zshrc && cp zshrc $HOME/.zshrc && sudo cp nanorc.conf /etc/nanorc && source $HOME/.zshrc
